a5120783fd631d5b9aab9baa7a5a8b8276c7105b
Paweł Matysiak <mat.pawel@gmail.com>
2020-11-17 18:04:57 +0100

    Create app layout

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 
-let isLogged = false;
+let isLogged = true;
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
+
 
 app.use('/user', (req, res, next) => {
   if(isLogged) next();
@@ -20,22 +19,38 @@ app.use('/user', (req, res, next) => {
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about');
 });
 app.get('/user/settings', (req, res) => {
-  res.show('user_settings.html');
+  res.render('user_settings');
 });
 app.get('/user/panel', (req, res) => {
-  res.show('user_panel.html');
+  res.render('user_panel');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { layout: false, name: req.params.name });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
 });
 
 app.use((req, res) => {
-  res.status(404).send('<h1>Nothing here...</h1><br><img src="tenor.gif">');
-})
+  res.status(404).render('404');
+});
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
